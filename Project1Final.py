00f342d05c84b386bd988e9511782afa8958b225
Panos-laptop <pan_evange@yahoo.com>
2020-04-03 23:36:00 +0300

    first exploration of all csv files in all folders of months

--- Project1Final.py
+++ Project1Final.py
@@ -7,8 +7,8 @@
 #     text_representation:
 #       extension: .py
 #       format_name: light
-#       format_version: '1.4'
-#       jupytext_version: 1.1.1
+#       format_version: '1.5'
+#       jupytext_version: 1.4.1
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -17,16 +17,82 @@
 
 # # <center>Data Mining Project 1 Spring semester 2019-2020</center>
 # ## <center>Παναγιώτης Ευαγγελίου &emsp; 1115201500039</center>
-# ## <center>Γεώργιος Μαραγκοζάκης &emsp;&emsp;&emsp;&ensp; 1115201500089</center>
+# ## <center>Γεώργιος Μαραγκοζάκης &emsp; 1115201500089</center>
 
 # ___
 
 # ### Do all the necessary imports for this notebook
 
-# for wordclouds
-from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+# region
+# for data exploration
 import pandas as pd
-from wordcloud import WordCloud
-from IPython.display import Image
-from PIL import Image as imgWordcloud
-import numpy as np
+import os
+from IPython.display import display
+
+# for something else
+# endregion
+
+# ## __Data Exploration__
+
+# - ### *Make one csv with the specific columns*
+
+# region
+myMonthsFolder = ['febrouary','march','april']
+dataPathDir = './data/'
+
+allMonthDf = []
+
+# for each month
+for month in myMonthsFolder:
+    print('Folder ' + month + ':')
+    specificPath = dataPathDir + month + '/'
+
+    # list of dataframes of this month
+    monthFilesDfList = []
+
+    # find the column's names of each csv
+    for fileName in os.listdir(specificPath):
+        # we need to check only .csv files
+        if fileName.endswith(".csv"):
+            thisCsv = pd.read_csv(os.path.join(specificPath, fileName), dtype='unicode')
+            
+            # make a list of columns of this file 
+            columnsList = [x for x in thisCsv.columns]
+
+            # make columns df for this file
+            d = {fileName:columnsList}
+            df = pd.DataFrame(data=d)
+            
+            # append this df to monthFilesDfList
+            monthFilesDfList.append(df)
+        else:
+            continue
+
+    concatDf = pd.concat(monthFilesDfList, axis=1)
+    concatDf = concatDf.fillna('-')
+    concatDf.name = month
+    display(concatDf)
+
+    allMonthDf.append(concatDf)
+
+# check if corresponding files in months' folders have the same column names
+allAreEqual = True
+curentDf = allMonthDf[0]
+for x in allMonthDf[1:]:
+    if not curentDf.equals(x):
+        allAreEqual = False
+        break
+
+if allAreEqual:
+    print('Column names in corresponding files in months\' folders are the same.')
+else:
+    print('Column names in corresponding files in months\' folders are not the same.')
+# endregion
+
+# Παρατηρούμε ότι:
+# - Τα αντίστοιχα αρχεία σε όλους τους φακέλους των μηνών έχουν τα ίδια ονόματα στηλών
+# - Τα περισσότερα .csv αρχεία έχουν σχετικά λίγες στήλες εκτός από το αρχείο listings.csv το οποίο
+# έχει πολλές στήλες οπότε είναι δύσκολο να βρούμε "με το μάτι" τις στήλες που ζητούνται 
+
+# __Let's find which file has each of the specific columns that we need. As column
+# names in corresponding files in months' folders are the same we just need to check only one folder.__
